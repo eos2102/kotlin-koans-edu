0f11ebfe7f0b555e996fd96eb5b4fb59c142e644
Phillipa Avery <pippa@netflix.com>
2018-09-23 11:07:31 -0700

    Fix for issue: https://github.com/Kotlin/kotlin-koans-edu/issues/14 where the direct conversion copy and paste fails the current test

--- Kotlin Koans/Introduction/Java to Kotlin conversion/Solution.kt
+++ Kotlin Koans/Introduction/Java to Kotlin conversion/Solution.kt
@@ -1,18 +1,20 @@
 package introduction.java_to_kotlin_conversion
 
-/*<taskWindow>*/fun toJSON(collection: Collection<Int>): String {
-    val sb = StringBuilder()
-    sb.append("[")
-    val iterator = collection.iterator()
-    while (iterator.hasNext())
-    {
-        val element = iterator.next()
-        sb.append(element)
-        if (iterator.hasNext())
+/*<taskWindow>*/class JavaCode {
+    fun toJSON(collection:Collection<Int>):String {
+        val sb = StringBuilder()
+        sb.append("[")
+        val iterator = collection.iterator()
+        while (iterator.hasNext())
         {
-            sb.append(", ")
+            val element = iterator.next()
+            sb.append(element)
+            if (iterator.hasNext())
+            {
+                sb.append(", ")
+            }
         }
+        sb.append("]")
+        return sb.toString()
     }
-    sb.append("]")
-    return sb.toString()
 }/*</taskWindow>*/
--- Kotlin Koans/Introduction/Java to Kotlin conversion/Test.kt
+++ Kotlin Koans/Introduction/Java to Kotlin conversion/Test.kt
@@ -1,11 +1,11 @@
 package introduction.java_to_kotlin_conversion
 
-import org.junit.Test
-import org.junit.Assert
 import koans.util.toMessageInEquals
+import org.junit.Assert
+import org.junit.Test
 
 class TestFunctions() {
     @Test fun collection() {
-        Assert.assertEquals("toJSON".toMessageInEquals(), "[1, 2, 3, 42, 555]", toJSON(listOf(1, 2, 3, 42, 555)))
+        Assert.assertEquals("toJSON".toMessageInEquals(), "[1, 2, 3, 42, 555]", JavaCode().toJSON(listOf(1, 2, 3, 42, 555)))
     }
 }
